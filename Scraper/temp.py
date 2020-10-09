6328afb58c4ecc5e27ffe9d4871de6df6d71c608
swang1112 <59112676+swang1112@users.noreply.github.com>
2020-10-09 11:50:26 +0200

    fig

--- Scraper/temp.py
+++ Scraper/temp.py
@@ -1,11 +1,27 @@
-from bs4 import BeautifulSoup
+from datetime import datetime
 import pandas as pd
-import webbot
+import requests
 
-my_url = "https://alternative.me/crypto/fear-and-greed-index/"
-my_web = webbot.Browser()
-my_web.go_to(my_url)
 
-# crawler
-page = my_web.get_page_source()
-soup = BeautifulSoup(page, "html.parser")
+def main():
+    my_url = "https://api.alternative.me/fng/?limit=0" 
+    response = requests.get(my_url)
+    print(response.status_code)
+
+    my_data = response.json()["data"]
+
+    date = []
+    value = []
+    for i in my_data:
+        date_temp = datetime.utcfromtimestamp(int(i["timestamp"])).strftime('%Y-%m-%d')
+        date.append(date_temp)
+        value.append(i["value"])
+
+    output = {"date": date, "value": value}
+    output_df = pd.DataFrame(output)
+
+    return output_df
+
+if __name__ == "__main__":
+    df = main()
+    df.to_csv(file_name, header=True)
